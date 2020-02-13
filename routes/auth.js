b5c675a53f89b1065e18a6e69794895517dac4aa
Cole Johnson <cburnjohnson@gmail.com>
2020-02-13 14:36:05 -0800

    auth middleware/login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
 
 // @route  GET /api/auth
 // @desc   Get logged in User
@@ -18,10 +21,40 @@ router.post(
         check('email', 'Valid email is required.').isEmail(),
         check('password', 'Valid password is required').exists()
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ msg: 'User is not found!' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ msg: 'Invalid Credentials' });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ msg: 'Server Error' });
         }
     }
 );
