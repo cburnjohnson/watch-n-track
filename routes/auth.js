f523c88a14869237f39b7abd3cc852b86b29474f
Cole Johnson <cburnjohnson@gmail.com>
2020-02-13 14:41:17 -0800

    auth get logged in user route

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
 // @route  GET /api/auth
 // @desc   Get logged in User
 // @access Private
-router.get('/', (req, res) => {
-    res.send('gets logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server Error' });
+    }
 });
 
 // @route   POST /api/auth
