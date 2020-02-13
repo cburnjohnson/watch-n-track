503b10a08a24a3539435a08dfc1f011731608ffa
Cole Johnson <cburnjohnson@gmail.com>
2020-02-13 12:53:21 -0800

    Tv show schema

---
+++ models/TvShow.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const TvShowSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    season: {
+        type: String,
+    },
+    episode: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = mongoose.model('tvshow', TvShowSchema)
