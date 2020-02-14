bc8f0d835fd033512bd61c690d30158355285b24
Cole Johnson <cburnjohnson@gmail.com>
2020-02-14 14:45:16 -0800

    create new tv show route

--- routes/tvshows.js
+++ routes/tvshows.js
@@ -20,3 +20,44 @@ router.get('/', auth, async (req, res) => {
         res.status(500).json({ msg: 'Server Error' });
     }
 });
+
+// @route   POST /api/tvshows
+// @desc    Add new tv show
+// @access  Private
+router.post(
+    '/',
+    [
+        auth,
+        [
+            check('name', 'TV show name is required.')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, season, episode } = req.body;
+
+        try {
+            const newTvShow = new TvShow({
+                name,
+                season,
+                episode,
+                user: req.user.id
+            });
+
+            const tvShow = await newTvShow.save();
+
+            res.json(tvShow);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ msg: 'Server Error' });
+        }
+    }
+);
+
+module.exports = router;
