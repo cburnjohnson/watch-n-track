d718bd1509ca3ac1abda332bd10e56fce140f139
Cole Johnson <cburnjohnson@gmail.com>
2020-02-16 16:47:10 -0800

    linked movies route

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tvshows', require('./routes/tvshows'));
 app.use('/api/animes', require('./routes/animes'));
+app.use('/api/movies', require('./routes/movies'));
 
 const PORT = process.env.PORT || 5000;
 
