3abdf746f32a8723a9176bb0eefe13fb2773c949
Cole Johnson <cburnjohnson@gmail.com>
2020-02-16 16:32:55 -0800

    movie get route functionality

--- routes/movies.js
+++ routes/movies.js
@@ -9,28 +9,35 @@ const Movie = require('../models/Movie');
 // @route   GET /api/movies
 // @desc    Get users movies
 // @access  Private
-router.get('/', auth, (req, res) => {
-    res.send('get users movies');
+router.get('/', auth, async (req, res) => {
+    try {
+        const movies = await Movie.findById(req.user.id).sort({ date: -1 });
+
+        res.json(movies);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server Error' });
+    }
 });
 
 // @route   POST /api/movies
 // @desc    Adds a new movie
 // @access  Private
-router.post('/', auth, (req, res) => {
+router.post('/', auth, async (req, res) => {
     res.send('add new movie');
 });
 
 // @route   DELETE /api/movies/:id
 // @desc    Deletes a movie
 // @access  Private
-router.delete('/', auth, (req, res) => {
+router.delete('/', auth, async (req, res) => {
     res.send('delete a movie');
 });
 
 // @roue    PUT /api/movies/:id
 // @desc    Updates a movie
 // @access  Private
-router.put('/', auth, (req, res) => {
+router.put('/', auth, async (req, res) => {
     res.send('update movie');
 });
 
