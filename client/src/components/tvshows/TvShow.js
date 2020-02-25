0307a39614783e3a70ac50b9cbf5299a0cd91c8d
Cole Johnson <cburnjohnson@gmail.com>
2020-02-24 21:03:59 -0800

    enter update functionality

--- client/src/components/tvshows/TvShow.js
+++ client/src/components/tvshows/TvShow.js
@@ -5,7 +5,7 @@ import TvShowContext from '../../context/tvshow/tvShowContext';
 const TvShow = ({ tvShow }) => {
     const tvShowContext = useContext(TvShowContext);
 
-    const { deleteTvShow } = tvShowContext;
+    const { deleteTvShow, updateTvShow } = tvShowContext;
 
     const { _id, name, season, episode } = tvShow;
 
@@ -32,11 +32,41 @@ const TvShow = ({ tvShow }) => {
         deleteTvShow(_id);
     };
 
+    const onBlur = e => {
+        e.target.removeAttribute('tabIndex');
+        e.target.contentEditable = false;
+        e.target.classList.remove('current-edit');
+        updateTvShow();
+    };
+
+    const onKeyPress = e => {
+        if (e.target) {
+            const currentEl = e.target;
+            if (e.charCode === 13 && document.activeElement === currentEl) {
+                e.preventDefault();
+                document.activeElement.contentEditable = false;
+                if (document.activeElement.tabIndex === 3) {
+                    document.activeElement.blur();
+                }
+                if (document.activeElement.nextSibling !== null) {
+                    document.activeElement.nextSibling.focus();
+                }
+                updateTvShow();
+            }
+        }
+    };
+
     return (
         <>
-            <span onFocus={onFocus}>{name}</span>
-            <span onFocus={onFocus}>{season}</span>
-            <span onFocus={onFocus}>{episode}</span>
+            <span onKeyPress={onKeyPress} onBlur={onBlur} onFocus={onFocus}>
+                {name}
+            </span>
+            <span onKeyPress={onKeyPress} onBlur={onBlur} onFocus={onFocus}>
+                {season}
+            </span>
+            <span onKeyPress={onKeyPress} onBlur={onBlur} onFocus={onFocus}>
+                {episode}
+            </span>
             <div>
                 <i className='edit-icon fas fa-edit' onClick={onUpdate}></i>
                 <i className='delete-icon fas fa-trash' onClick={onDelete}></i>
