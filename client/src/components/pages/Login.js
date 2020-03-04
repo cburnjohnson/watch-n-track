220ac61b6fa70c9c67f195b424226a21a5589a12
Cole Johnson <cburnjohnson@gmail.com>
2020-03-04 14:23:33 -0800

    login form styling

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const Login = () => {
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log('logged in');
+    };
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='form-container'>
+            <h1>
+                Log<span className='highlight'>in</span>
+            </h1>
+            <form onSubmit={onSubmit} className='flex-form login-form'>
+                <div className='form-group'>
+                    <label htmlFor='email'>Email Address</label>
+                    <input type='email' name='email' required />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        type='password'
+                        name='password'
+                        required
+                        minLength='6'
+                    />
+                </div>
+
+                <div className='form-group'>
+                    <input type='submit' value='Log In' />
+                </div>
+            </form>
         </div>
     );
 };
