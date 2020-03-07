64eb82d2b61412eeb03c2a15736163198a441ed2
Cole Johnson <cburnjohnson@gmail.com>
2020-03-07 14:37:48 -0800

    Alerts component

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ import Navbar from './components/layout/Navbar';
 import Register from './components/pages/Register';
 import Login from './components/pages/Login';
 import PrivateRoute from './components/auth/PrivateRoute';
+import Alert from './components/layout/Alerts';
 
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import MovieState from './context/movie/MovieState';
 import TvShowState from './context/tvshow/TvShowState';
 import AnimeState from './context/anime/AnimeState';
@@ -23,18 +25,29 @@ function App() {
             <MovieState>
                 <TvShowState>
                     <AnimeState>
-                        <Router>
-                            <Navbar />
-                            <Switch>
-                                <PrivateRoute exact path='/' component={Home} />
-                                <Route
-                                    exact
-                                    path='/register'
-                                    component={Register}
-                                />
-                                <Route exact path='/login' component={Login} />
-                            </Switch>
-                        </Router>
+                        <AlertState>
+                            <Router>
+                                <Navbar />
+                                <Alerts />
+                                <Switch>
+                                    <PrivateRoute
+                                        exact
+                                        path='/'
+                                        component={Home}
+                                    />
+                                    <Route
+                                        exact
+                                        path='/register'
+                                        component={Register}
+                                    />
+                                    <Route
+                                        exact
+                                        path='/login'
+                                        component={Login}
+                                    />
+                                </Switch>
+                            </Router>
+                        </AlertState>
                     </AnimeState>
                 </TvShowState>
             </MovieState>
