dfc6ea04d4cc6a272c38f801cc864a985c036f97
Cole Johnson <cburnjohnson@gmail.com>
2020-03-08 17:37:44 -0700

    Deployment preparation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect to DataBase
 connectDB();
@@ -15,6 +16,16 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tvshows', require('./routes/tvshows'));
 app.use('/api/anime', require('./routes/anime'));
 app.use('/api/movies', require('./routes/movies'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
